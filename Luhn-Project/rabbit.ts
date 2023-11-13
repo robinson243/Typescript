38ece234afec9f615ed649ca292e9908aae609c8
mukena_r <mukena_r@etna-alternance.net>
2023-11-13 15:33:13 +0100

    Luhn Project

---
+++ Luhn-Project/rabbit.ts
@@ -0,0 +1,23 @@
+export function bunnyroad(instructions:string[]){
+    let x: number = 0;
+    let y: number = 0;
+    for (let ins of instructions) {
+        if (ins == 'gauche'){
+            x--;
+        }
+        else if (ins == 'droite') {
+            x++;
+        }
+        else if (ins == 'nord') {
+            y++;
+        }
+        else if (ins == 'sud') {
+            y--;
+        }
+    }
+    return {x, y};
+}
+
+// const instructions = ['droite', 'droite', 'gauche', 'droite', 'nord', 'sud', 'nord'];
+// const positionFinale = bunnyroad(instructions);
+// console.log(`Le lapin est à la position (${positionFinale.x}, ${positionFinale.y})`);
